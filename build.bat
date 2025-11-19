c753dc04797865203b242ae676098f40b769e809
Sjanoki <57033790+Sjanoki@users.noreply.github.com>
2025-11-19 15:57:58 +0700

    Simplify build.bat for Debug configuration only

--- build.bat
+++ build.bat
@@ -1,22 +1,12 @@
 @echo off
-setlocal enableextensions
 cd /d "%~dp0"
 
-rem Optional first argument: build type (Debug or Release)
-set "BUILD_TYPE=%~1"
-
-if "%BUILD_TYPE%"=="" (
-    set /p BUILD_TYPE=Build type [Debug/Release] (default Debug): 
-)
-
-if "%BUILD_TYPE%"=="" set "BUILD_TYPE=Debug"
-
-echo Configuring build (%BUILD_TYPE%)...
+echo Configuring Debug build...
 cmake -S . -B build
 
-echo Building...
-cmake --build build --config %BUILD_TYPE%
+echo Building Debug...
+cmake --build build --config Debug
 
 echo.
-echo Build finished. Press any key to continue . . .
+echo Debug build finished. Press any key to continue . . .
 pause
